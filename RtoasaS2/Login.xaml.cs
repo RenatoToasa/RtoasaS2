50c7ffd275825b2d504efdc62236ce470ad87304
Renato Toasa <renato_toasa@hotmail.com>
2022-10-19 19:12:07 -0500

    Add project files.

---
+++ RtoasaS2/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RtoasaS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+
+         //   Navigation.PushAsync(new Registro(2,2));
+            string usuario = "rtoasa";
+            string contrasena = "12345";
+
+            //  string tUsuario = txtUsuario.Text;
+            //   string tContrasena = txtContrasena.Text;
+            /*
+               if (usuario == tUsuario & contrasena == tContrasena)
+               {
+                   DisplayAlert("Alerta", "USUARIO CORRECTO", "Cerrar");
+
+                   Navigation.PushAsync(new Registro());
+               }
+               else
+               {
+                   DisplayAlert("Alerta", "USUARIO NO EXISTE", "Cerrar");
+               }
+            */
+        }
+
+    }
+}
---
+++ RtoasaS2/Registro.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RtoasaS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario, string contrasena)
+        {
+            InitializeComponent();
+            lblUsuario.Text ="El usuario conectado es "+ usuario +" la contrasena es "+ contrasena;
+        }
+
+        private void btnInformacion_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            double nota = Convert.ToDouble(txtNota.Text);
+
+            if (nota >= 7)
+            {
+
+                DisplayAlert("Mensaje de alerta", "Tu nombre es " + nombre + "\nTu apellido es " + apellido + " \nAPROBASTE con " + nota, "Cerrar");
+            }
+            else
+            {
+                DisplayAlert("Mensaje de alerta", "Tu nombre es " + nombre + "Tu apellido es " + apellido + " Reprobaste con "+nota, "Cerrar");
+
+            }
+
+        }
+    }
+}
